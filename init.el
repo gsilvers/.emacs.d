e044c50de70c0480353e9829134621d15cde363d
Greg.Silverstein <greg.silverstein@creditsafe.com>
2022-01-03 11:18:29 -0500

    add powershell

--- init.el
+++ init.el
@@ -11,8 +11,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2)
  '(package-selected-packages
-   '(multiple-cursors nyan-mode magit helpful counsel ivy-rich which-key rainbow-delimiters doom-themes doom-modeline ivy command-log-mode use-package)))
+   '(powershell multiple-cursors nyan-mode magit helpful counsel ivy-rich which-key rainbow-delimiters doom-themes doom-modeline ivy command-log-mode use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
