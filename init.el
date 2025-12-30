fec91ce985fd86716b4728350ef1143d746cd630
Greg Silverstein <greg.silverstein@gmail.com>
2025-12-29 22:26:02 -0500

    add some features from work

--- init.el
+++ init.el
@@ -37,6 +37,7 @@
 	consult
 	embark
 	embark-consult
+	use-package
 	corfu))
 
     (dolist (pkg my/packages)
@@ -111,8 +112,52 @@ a line to the file with today's date."
 	  (append-to-file (point-min) (point-max) file))
 	(message "Logged ride on %s." date)))
 
-    (global-set-key (kbd "<volume-up>") 'scroll-down-command)
-    (global-set-key (kbd "<volume-down>") 'scroll-up-command)
+    (use-package magit
+      :bind (("C-x g" . magit-status)
+	     ("C-x C-g" . magit-status))
+      :custom
+      (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+    (use-package markdown-mode
+      :ensure t
+      :mode ("README\\.md\\'" . gfm-mode)
+      :init (setq markdown-command "multimarkdown")
+      :bind (:map markdown-mode-map
+		  ("C-c C-e" . markdown-do)))
+
+    
+    (use-package selected-window-accent-mode
+      :ensure t 
+      :config (selected-window-accent-mode 1)
+      :custom
+      (selected-window-accent-fringe-thickness 10)
+      (selected-window-accent-custom-color "#CC5500")
+      (selected-window-accent-mode-style 'subtle))
+
+
+    ;; projectile basics
+    (use-package projectile
+      :diminish projectile-mode
+      :config (projectile-mode)
+      :bind-keymap
+      ("C-c p" . projectile-command-map)
+      :init
+      ;; NOTE: Set this to the folder where you keep your Git repos!
+      (when (file-directory-p "~/")
+	(setq projectile-project-search-path '("~/")))
+      (setq projectile-switch-project-action #'projectile-dired))
+
+
+    ;; perspective mode
+    (use-package perspective
+      :ensure t ; use `:straight t` if using straight.el!
+      :bind
+      ("C-x C-b" . persp-list-buffers)
+      :custom
+      (persp-mode-prefix-key (kbd "C-x C-p"))
+      :init
+      (persp-mode))
+
 
     ))
 ;;; End Universal Setup
@@ -124,6 +169,132 @@ a line to the file with today's date."
   "Items to be executed when logged in on macos only"
   (progn
     (set-desktop-size)
+
+    (use-package vterm
+      :commands vterm
+      :config
+      (setq term-prompt-regexp ".*>\s\]")
+      (setq vterm-max-scrollback 10000))
+
+    (use-package consult
+      ;; Replace bindings. Lazily loaded due by `use-package'.
+      :bind (;; C-c bindings in `mode-specific-map'
+	     ("C-c M-x" . consult-mode-command)
+	     ("C-c h" . consult-history)
+	     ("C-c k" . consult-kmacro)
+	     ("C-c m" . consult-man)
+	     ("C-c i" . consult-info)
+	     ([remap Info-search] . consult-info)
+	     ;; C-x bindings in `ctl-x-map'
+	     ("C-x M-:" . consult-complex-command) ;; orig. repeat-complex-command
+	     ("C-x b" . consult-buffer) ;; orig. switch-to-buffer
+	     ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+	     ("C-x 5 b" . consult-buffer-other-frame) ;; orig. switch-to-buffer-other-frame
+	     ("C-x t b" . consult-buffer-other-tab) ;; orig. switch-to-buffer-other-tab
+	     ("C-x r b" . consult-bookmark) ;; orig. bookmark-jump
+	     ("C-x p b" . consult-project-buffer) ;; orig. project-switch-to-buffer
+	     ;; Custom M-# bindings for fast register access
+	     ("M-#" . consult-register-load)
+	     ("M-'" . consult-register-store) ;; orig. abbrev-prefix-mark (unrelated)
+	     ("C-M-#" . consult-register)
+	     ;; Other custom bindings
+	     ("M-y" . consult-yank-pop) ;; orig. yank-pop
+	     ;; M-g bindings in `goto-map'
+	     ("M-g e" . consult-compile-error)
+	     ("M-g f" . consult-flymake) ;; Alternative: consult-flycheck
+	     ("M-g g" . consult-goto-line) ;; orig. goto-line
+	     ("M-g M-g" . consult-goto-line) ;; orig. goto-line
+	     ("M-g o" . consult-outline) ;; Alternative: consult-org-heading
+	     ("M-g m" . consult-mark)
+	     ("M-g k" . consult-global-mark)
+	     ("M-g i" . consult-imenu)
+	     ("M-g I" . consult-imenu-multi)
+	     ;; M-s bindings in `search-map'
+	     ("M-s d" . consult-find) ;; Alternative: consult-fd
+	     ("M-s c" . consult-locate)
+	     ("M-s g" . consult-grep)
+	     ("M-s G" . consult-git-grep)
+	     ("M-s r" . consult-ripgrep)
+	     ("M-s l" . consult-line)
+	     ("M-s L" . consult-line-multi)
+	     ("M-s k" . consult-keep-lines)
+	     ("M-s u" . consult-focus-lines)
+	     ;; Isearch integration
+	     ("M-s e" . consult-isearch-history)
+	     :map isearch-mode-map
+	     ("M-e" . consult-isearch-history) ;; orig. isearch-edit-string
+	     ("M-s e" . consult-isearch-history) ;; orig. isearch-edit-string
+	     ("M-s l" . consult-line) ;; needed by consult-line to detect isearch
+	     ("M-s L" . consult-line-multi) ;; needed by consult-line to detect isearch
+	     ;; Minibuffer history
+	     :map minibuffer-local-map
+	     ("M-s" . consult-history) ;; orig. next-matching-history-element
+	     ("M-r" . consult-history)) ;; orig. previous-matching-history-element
+
+      ;; Enable automatic preview at point in the *Completions* buffer. This is
+      ;; relevant when you use the default completion UI.
+      :hook (completion-list-mode . consult-preview-at-point-mode)
+
+      ;; The :init configuration is always executed (Not lazy)
+      :init
+
+      ;; Optionally configure the register formatting. This improves the register
+      ;; preview for `consult-register', `consult-register-load',
+      ;; `consult-register-store' and the Emacs built-ins.
+      (setq register-preview-delay 0.5
+	    register-preview-function #'consult-register-format)
+
+      ;; Optionally tweak the register preview window.
+      ;; This adds thin lines, sorting and hides the mode line of the window.
+      (advice-add #'register-preview :override #'consult-register-window)
+
+      ;; Use Consult to select xref locations with preview
+      (setq xref-show-xrefs-function #'consult-xref
+	    xref-show-definitions-function #'consult-xref)
+
+      ;; Configure other variables and modes in the :config section,
+      ;; after lazily loading the package.
+      :config
+
+      ;; Optionally configure preview. The default value
+      ;; is 'any, such that any key triggers the preview.
+      ;; (setq consult-preview-key 'any)
+      ;; (setq consult-preview-key "M-.")
+      ;; (setq consult-preview-key '("S-<down>" "S-<up>"))
+      ;; For some commands and buffer sources it is useful to configure the
+      ;; :preview-key on a per-command basis using the `consult-customize' macro.
+      (consult-customize
+       consult-theme :preview-key '(:debounce 0.2 any)
+       consult-ripgrep consult-git-grep consult-grep
+       consult-bookmark consult-recent-file consult-xref
+       consult--source-bookmark consult--source-file-register
+       consult--source-recent-file consult--source-project-recent-file
+       ;; :preview-key "M-."
+       :preview-key '(:debounce 0.4 any))
+
+      ;; Optionally configure the narrowing key.
+      ;; Both < and C-+ work reasonably well.
+      (setq consult-narrow-key "<") ;; "C-+"
+
+      ;; Optionally make narrowing help available in the minibuffer.
+      ;; You may want to use `embark-prefix-help-command' or which-key instead.
+      ;; (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
+
+      ;; By default `consult-project-function' uses `project-root' from project.el.
+      ;; Optionally configure a different project root function.
+;;;; 1. project.el (the default)
+      ;; (setq consult-project-function #'consult--default-project--function)
+;;;; 2. vc.el (vc-root-dir)
+      ;; (setq consult-project-function (lambda (_) (vc-root-dir)))
+;;;; 3. locate-dominating-file
+      ;; (setq consult-project-function (lambda (_) (locate-dominating-file "." ".git")))
+;;;; 4. projectile.el (projectile-project-root)
+      ;; (autoload 'projectile-project-root "projectile")
+      ;; (setq consult-project-function (lambda (_) (projectile-project-root)))
+;;;; 5. No project support
+      ;; (setq consult-project-function nil)
+      )
+
     ))
 ;;; End Macos Setup
 ;;; ====================================================================================
@@ -143,6 +314,10 @@ a line to the file with today's date."
     (modifier-bar-mode 1)
     (menu-bar-set-tool-bar-position `bottom)
     (set-mobile-size)
+    
+    (global-set-key (kbd "<volume-up>") 'scroll-down-command)
+    (global-set-key (kbd "<volume-down>") 'scroll-up-command)
+    
     ))
 ;;; End Android Setup
 ;;; ====================================================================================
@@ -174,7 +349,9 @@ a line to the file with today's date."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(corfu ef-themes embark-consult marginalia markdown-mode orderless
+	   perspective selected-window-accent-mode vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
